0a6a60792696a6b533225b2a7b2ece737003eda1
sechiyo97 <sechiyo97@gmail.com>
2020-01-26 18:48:28 +0900

    1.4.0버전 (새로운 숙제 추가, 일간/주간 초기화 나누기)

--- app/src/main/java/com/queserasera/lostarkhomework/MariActivity.java
+++ app/src/main/java/com/queserasera/lostarkhomework/MariActivity.java
@@ -9,13 +9,12 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 
